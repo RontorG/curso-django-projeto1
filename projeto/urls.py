1814f70fa20b52fe5d49df8330544ee2691ec23a
Lucas Pereira <lucasspp2003@outlook.com>
2022-12-04 13:26:17 -0300

    removido contato e sobre

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from recipes.views import home, contato, sobre
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- recipes/views.py
+++ recipes/views.py
@@ -6,11 +6,4 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'recipes/home.html')
 
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(requet):
-    return HttpResponse('sobre')
-
 
