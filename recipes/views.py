fa6e1160cf86b730246aedbc201a8c27060977b3
Lucas Pereira <lucasspp2003@outlook.com>
2022-12-18 23:28:22 -0300

    Injetando dados no context da view para testes

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from utils.recipes.factory import makeRecipe
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes' : [makeRecipe() for _ in range(10)],
+        })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
-
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe' : makeRecipe(),
+        })
 
