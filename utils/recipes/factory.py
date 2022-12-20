ca817aec5b548cf8cc083be9042c1acf6979f2b0
Lucas Pereira <lucasspp2003@outlook.com>
2022-12-19 23:56:18 -0300

    URLs dinâmicas para as views

--- utils/recipes/factory.py
+++ utils/recipes/factory.py
@@ -12,6 +12,7 @@ fake = Faker('pt_BR')
 def makeRecipe():
     return {
             'title' : fake.sentence(nb_words=6),
+            'id' : fake.random_number(digits=2, fix_len=True),
             'description' : fake.sentence(nb_words=13),
             'preparation_time' : fake.random_number(digits=2, fix_len=True),
             'preparation_time_unit' : 'Minutos',
