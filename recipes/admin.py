a19dd43c5655eb82061596e6bdee2756a53532f3
Lucas Pereira <lucasspp2003@outlook.com>
2022-12-25 20:49:15 -0300

    registrado models em admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+# importar as models
+from .models import Category, Recipe
+
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+#admin.site.register(Category, CategoryAdmin)
+
