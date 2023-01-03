ae980eb32bfaa527db5d1afa271c10f723e44315
Lucas Pereira <lucasspp2003@outlook.com>
2023-01-02 22:57:58 -0300

    Campo de categoria para receitas pode ficar em branco

--- recipes/models.py
+++ recipes/models.py
@@ -24,7 +24,10 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(
+            Category, on_delete=models.SET_NULL, null=True, blank=True,
+            default=None
+            )
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
 
