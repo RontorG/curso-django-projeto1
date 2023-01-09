ca5d865c8fba8e852e73fab42ea188ec23b4ddeb
Lucas Pereira <lucasspp2003@outlook.com>
2023-01-08 23:15:12 -0300

    Erro 404 para páginas de categorias que não exixtem

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_list_or_404, get_object_or_404
 from utils.recipes.factory import makeRecipe
 from .models import Recipe
 
@@ -8,18 +8,22 @@ def home(request):
     recipes = Recipe.objects.filter(
             is_published=True
             ).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
     'recipes' : recipes,
     })
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-            category__id=category_id,
-            is_published=True
-            ).order_by('-id')
+    recipes = get_list_or_404(
+            Recipe.objects.filter(
+                category__id=category_id,
+                is_published=True
+            ).order_by('-id'))
+            
     return render(request, 'recipes/pages/category.html', context={
     'recipes' : recipes,
+    'category' : f'{recipes[0].category.name.capitalize()} - Category',
     })
 
 
